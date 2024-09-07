e1b0f4f128c9f1e38a0cdac5062d2dae4c939428
TingChiehLin <cooloojayoo@gmail.com>
2024-09-08 02:43:17 +1000

    Add - Set up use client

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { Jost } from "next/font/google";
 
 import { FeedbackProvider } from "@/store/product-feedback-context";
+import { FeedbackQuery } from "@/query/queryclient";
+
 import Footer from "@/layouts/Footer";
 
 const jost = Jost({
@@ -20,10 +22,12 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html lang="en" className={`${jost.className} h-full`}>
       <head />
       <body className="min-h-screen grid grid-rows-[auto,1fr,auto] bg-pfGrayLight leading-loose tracking-wider">
-        <FeedbackProvider>
-          <main className="w-full max-w-7xl py-24 mx-auto">{children}</main>
-          <Footer />
-        </FeedbackProvider>
+        <FeedbackQuery>
+          <FeedbackProvider>
+            <main className="w-full max-w-7xl py-24 mx-auto">{children}</main>
+            <Footer />
+          </FeedbackProvider>
+        </FeedbackQuery>
       </body>
     </html>
   );
---
+++ client/query/queryclient.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as React from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export const FeedbackQuery = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
