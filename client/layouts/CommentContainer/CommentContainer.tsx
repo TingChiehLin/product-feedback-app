666c1850f315cea49df3daaf56ef28e9023dcd39
TingChiehLin <cooloojayoo@gmail.com>
2024-10-10 01:50:33 +1100

    Add - limit for character and textarea for input

--- client/layouts/CommentContainer/CommentContainer.tsx
+++ client/layouts/CommentContainer/CommentContainer.tsx
@@ -4,31 +4,71 @@ import { FeedbackContext } from "@/store/product-feedback-context";
 import Button from "@/components/Button";
 import { useParams } from "next/navigation";
 import TextField from "@/components/TextField";
+import Comment from "@/components/Comment";
+
+type FormValues = {
+  [key: string]: {
+    value: string;
+    error: string;
+  };
+};
 
 const CommentContainer = () => {
-  const [values, setValues] = React.useState({
+  const [values, setValues] = React.useState<FormValues>({
     "comment-description": {
       value: "",
       error: "",
     },
   });
+
   const params = useParams();
   const fbCtx = React.useContext(FeedbackContext);
   const id = String(params?.id as string);
   const feedbackItem = fbCtx.feedbacks.find((f) => String(f.id) === id);
-  const feedbackComs = feedbackItem?.comments.length;
+  const feedbackComs = feedbackItem?.comments;
 
-  const handleValue = () => {
-    // setValues()
+  const charLimit = 250;
+  const charactersRemaining =
+    charLimit - values["comment-description"].value.length;
+
+  const handleValue = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+
+    if (value.length > charLimit) {
+      setValues((preState) => ({
+        ...preState,
+        [name]: {
+          ...preState[name],
+          error: "You have reached the character limit.",
+        },
+      }));
+      return;
+    }
+
+    setValues((preState) => ({
+      ...preState,
+      [name]: {
+        value: value,
+        error: "",
+      },
+    }));
+  };
+
+  const handleClick = () => {
+    console.log("Output Value:", values);
   };
 
   return (
     <>
       <div className="py-6 px-8 mt-6 bg-white">
         <span className="font-bold text-[18px] text-pfBlueDark">
-          {feedbackComs} Comments
+          {feedbackComs?.length ?? 0} Comments
         </span>
-        <div>Comment items</div>
+        <div>
+          {feedbackComs?.map((c) => (
+            <Comment key={c.id} />
+          ))}
+        </div>
       </div>
       <div className="bg-white py-6 px-8 mt-6">
         <span className="text-[18px] font-bold text-left">Add Comment</span>
@@ -36,15 +76,22 @@ const CommentContainer = () => {
           id={"comment-description"}
           name={"comment-description"}
           value={values["comment-description"].value}
-          rows={4}
+          rows={3}
           cols={50}
           placeholder={"Type your comment here"}
           error={values["comment-description"].error}
           onChange={handleValue}
         />
         <div className="flex justify-between items-center mt-4">
-          <span className="text-pfGrayDark">250 Characters left</span>
-          <Button text={"Post Comment"} variant={"Add"} type={"button"} />
+          <span className="text-pfGrayDark">
+            {charactersRemaining >= 0 ? charactersRemaining : 0} Characters left
+          </span>
+          <Button
+            text={"Post Comment"}
+            variant={"Add"}
+            type={"button"}
+            onClick={handleClick}
+          />
         </div>
       </div>
     </>
